50024e0d97df6d875d6b8d7b20008ef724ea08d4
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-03-15 00:16:58 +0800

    feat(flake): add configuration for the VM

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "My NixOS flake";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.moliouli = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
