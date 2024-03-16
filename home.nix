f2751607b3cc0a0a0c43c99705d8682f815df961
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-03-17 01:07:53 +0800

    feat(home-manager): add xresources config

--- home.nix
+++ home.nix
@@ -4,5 +4,10 @@
   home.username = "linnovs";
   home.homeDirectory = "/home/linnovs";
 
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
   home.stateVersion = "23.11";
 }
