4bfe5a699db04b4c07645a70326ba94454638618
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-03-17 01:30:47 +0800

    fix(output): home-manager.users instead of user

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.user.linnovs = import ./home.nix;
+          home-manager.users.linnovs = import ./home.nix;
         }
       ];
     };
