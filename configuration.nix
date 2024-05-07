c0e48c1e8611087580451b316cc4f64d4b208272
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-05-07 21:52:14 +0800

    feat(config): enable avahi

--- configuration.nix
+++ configuration.nix
@@ -70,6 +70,8 @@
     };
   };
 
+  services.avahi.enable = true;
+
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
