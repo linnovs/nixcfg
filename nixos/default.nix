e8918e1d40de6b225dd6b282d0fadbca359b0b61
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-05-15 23:57:55 +0800

    feat(nixos): weekly garbrage collection

--- nixos/default.nix
+++ nixos/default.nix
@@ -10,6 +10,12 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.trusted-users = [ "linnovs" ];
+  nix.gc =
+    {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
 
   environment.systemPackages = with pkgs; [
     git
