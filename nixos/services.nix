685f7a0e39f139378707450c8db4871bea82b70c
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-05-16 01:19:59 +0800

    feat(nixos): enable pipewire (alsa,pulse)

--- nixos/services.nix
+++ nixos/services.nix
@@ -4,4 +4,11 @@
   services.avahi.enable = true;
   services.geoclue2.enable = true;
   services.printing.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 }
