6894aa9dd40f222d9c0dfce8fbbb58e5054b2f72
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-05-16 04:19:20 +0800

    feat(sddm): add theme catppuccin-macchiato

--- nixos/window-manager.nix
+++ nixos/window-manager.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
   services.xserver = {
@@ -9,6 +9,16 @@
     displayManager.sddm = {
       enable = true;
       autoNumlock = true;
+      theme = "catppuccin-macchiato";
     };
   };
+
+  environment.systemPackages = lib.mkAfter [
+    (pkgs.catppuccin-sddm.override {
+      flavor = "macchiato";
+      font = "Noto Sans";
+      fontSize = 12;
+      clockEnable = true;
+    })
+  ];
 }
