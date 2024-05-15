42300aa4dbee4748d0dbf765ba759a3a8099ce1e
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-05-16 00:45:42 +0800

    feat(nixos): group boot

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,8 +1,10 @@
 { pkgs, ... }:
 
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = false;
+  boot = {
+    loader.systemd-boot.enable = true;
+    efi.canTouchEfiVariables = false;
+  };
 
   networking.networkmanager.enable = true;
 
