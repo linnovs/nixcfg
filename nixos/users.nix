412af9f4368107f5243ed2afbbaaf657dc077780
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-06-16 12:41:23 +0800

    chore(users): update ssh authorized key

--- nixos/users.nix
+++ nixos/users.nix
@@ -7,7 +7,9 @@
       description = "linnovs";
       initialPassword = "capital-impose-june";
       extraGroups = [ "networkmanager" "wheel" ];
-      openssh.authorizedKeys.keys = [ ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDIjRPXhIGrNUtCIdlzEoDmSq8tvAnPKtlnAgkH1//Ho linnovs@blazarmist"
+      ];
     };
   };
 }
