b96fc2786eaea672b6cb518e106b5b1f7b7b2c76
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-06-16 12:40:05 +0800

    chore(moliuli): move to hosts

--- hosts/moliuli/default.nix
+++ hosts/moliuli/default.nix
@@ -2,8 +2,6 @@
 
 {
   imports = [
-    ../default.nix
-    ../window-manager.nix
     ./hardware-configuration.nix # hardware configuration for the qemu system
   ];
 
@@ -15,10 +13,6 @@
       PasswordAuthentication = false;
     };
   };
-
-  users.users.linnovs.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDIjRPXhIGrNUtCIdlzEoDmSq8tvAnPKtlnAgkH1//Ho linnovs@blazarmist"
-  ];
 
   networking.hostName = "moliuli";
 }
