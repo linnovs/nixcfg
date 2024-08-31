55251eb81175c8ba2cd456fed34a4c0b51a3a5ae
Linnovs Liu <fforwm_cynnwys@slmail.me>
2024-08-31 12:37:42 +0800

    feat(moliuli): enable hyprland

--- hosts/moliuli/default.nix
+++ hosts/moliuli/default.nix
@@ -17,6 +17,7 @@
   networking.hostName = "moliuli";
 
   windowManagers.qtile = true;
+  windowManagers.hyprland = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
